1c25b591a3b4aa8c83ccd25c990da9207596b106
Master Freeze <ansarizain1222@gmail.com>
2026-06-10 14:40:47 -0400

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,18 +1,10 @@
 const posts = [
   {
-    title: "Formal Lab Report",
-    section: "library",
-    date: "2026-06-10",
-    meta: "Testing document",
-    file: "library/Formal Lab Report.pdf"
-  },
-  
-  {
-    title: "Master Freeze",
-    section: "art",
-    date: "2026-06-10",
-    meta: "Digital artwork",
-    file: "artwork/raw.png"
+    title: "",
+    section: "",
+    date: "",
+    meta: "",
+    file: ""
   }
 
 ];
