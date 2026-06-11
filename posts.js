93878ea8a498827a443008d39a24d64cff7672b5
Master Freeze <ansarizain1222@gmail.com>
2026-06-10 23:54:04 -0400

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,10 +1,10 @@
 const posts = [
   {
-    title: "Coming Soon!",
-    section: "",
-    date: "2026-06-30",
-    meta: "",
-    file: ""
+    title: "Master Freeze Official Logo",
+    section: "art",
+    date: "2026-06-11",
+    meta: "Take a look at the legend himself.",
+    file: "artwork/raw.png"
   }
 
 ];
